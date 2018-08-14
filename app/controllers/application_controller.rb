3f6634a4c90763dcd38d0c98d2182be62dfff75e
theresamorelli <theresa.l.morelli@gmail.com>
2018-08-14 17:10:54 -0400

    add helper methods, use helpers to check for login status in some routes, added handling for different error message formats

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,31 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
+    redirect_if_logged_in(session)
     erb :welcome
   end
 
+  helpers do
+    def current_user(session)
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in?(session)
+      !!session[:user_id]
+    end
+
+    def redirect_if_not_logged_in(session)
+      if !logged_in?(session)
+        flash[:error] = "Please log in"
+        redirect to '/'
+      end
+    end
+
+    def redirect_if_logged_in(session)
+      if logged_in?(session)
+        redirect to '/index'
+      end
+    end
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,25 @@
 class UsersController < ApplicationController
 
   get '/signup' do
+    redirect_if_logged_in(session)
     erb :'users/signup'
+  end
+
+  get '/index' do
+    redirect_if_not_logged_in(session)
+    erb :'nuggets/index'
   end
 
   post '/signup' do
     new_user = User.new(params)
+    # if signup info is valid
     if new_user.save
+      # save new user to db
       new_user.save
+      # save user's info in session
       session[id: new_user.id]
-      redirect to '/nuggets'
+      # redirect to
+      redirect to '/index'
     else
       flash[:error] = new_user.errors.full_messages.uniq
       redirect to '/signup'
