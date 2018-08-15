47b17b20cb33a53ad2de7e56368aaab45d80efe5
theresamorelli <theresa.l.morelli@gmail.com>
2018-08-15 09:05:45 -0400

    added routes and functionality for nugget /show, /edit, and /delete

--- app/controllers/nuggets_controller.rb
+++ app/controllers/nuggets_controller.rb
@@ -1,7 +1,6 @@
 class NuggetsController < ApplicationController
 
   post '/nuggets' do
-    binding.pry
     # creates a new nugget, adds to current user
     current_user(session).nuggets.build(params).save
     redirect to 'index'
@@ -10,6 +9,24 @@ class NuggetsController < ApplicationController
   get '/nuggets/:id' do
     @nugget = Nugget.find_by(id: params[:id])
     erb :'nuggets/show'
+  end
+
+  get '/nuggets/:id/edit' do
+    @nugget = Nugget.find_by(id: params[:id])
+    erb :'nuggets/edit'
+  end
+  
+  delete '/nuggets/:id' do
+    Nugget.delete(params[:id])
+    redirect to '/index'
+  end
+
+  patch '/nuggets/:id' do
+    nugget = Nugget.find_by(id: params[:id])
+    nugget.content = params[:content]
+    nugget.public = params[:public]
+    nugget.save
+    redirect to "/nuggets/#{nugget.id}"
   end
 
 end
